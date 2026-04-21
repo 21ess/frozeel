f10115c99758b7cf5446da6b306a2077486d9c94
kjasn <iz.kjasn@icloud.com>
2026-04-21 17:33:44 +0800

    feat: add provider interface and bangumi instance

--- provider/bangumi/type.go
+++ provider/bangumi/type.go
@@ -1,1 +1,19 @@
+// Package bangumi provides a provider for Bangumi characters.
 package bangumi
+
+import (
+	"context"
+
+	"github.com/21ess/frozeel/provider"
+)
+
+type Provider struct{}
+
+func (b *Provider) GetRandomCharacter(ctx context.Context) (*provider.Character, error) {
+	// send post req
+	return nil, nil
+}
+
+func (b *Provider) GetCharacterByName(ctx context.Context, name string) (*provider.Character, error) {
+	return nil, nil
+}
--- provider/provider.go
+++ provider/provider.go
@@ -1,4 +1,20 @@
 package provider
 
+import "context"
+
 type AnimeProvider interface {
+	GetRandomCharacter(ctx context.Context) (*Character, error)
+	GetCharacterByName(ctx context.Context, name string) (*Character, error)
+	// GetCharacterTags(ctx context.Context, name string) ([]string, error)
+}
+
+// Character each game's answer, randomly picked from provider list
+type Character struct {
+	Name     string   `json:"name"`
+	Summary  string   `json:"summary"`
+	Image    string   `json:"image"` // default to large medium images
+	Gender   string   `json:"gender"`
+	Birthday string   `json:"birthday"`
+	Nsfw     bool     `json:"nsfw"` // R18 or not / hentai or regular
+	Tags     []string `json:"tags"` // TODO: how to set this?
 }
