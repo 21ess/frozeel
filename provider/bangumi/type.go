72592d042bcd2f467b9f503a37d973fc26c5aba5
kjasn <iz.kjasn@icloud.com>
2026-04-25 11:04:49 +0800

    refactor: provider interface and bangumi instance

--- provider/bangumi/type.go
+++ provider/bangumi/type.go
@@ -1,11 +1,26 @@
 // Package bangumi provides a provider for Bangumi characters.
 package bangumi
 
+const (
+	BaseUrl = "https://api.bgm.tv"
+)
+
+type BmProvider struct {
+	Token string
+}
+
 type bangumiSearchResponse struct {
 	Data   []bangumiCharacter `json:"data"`
 	Total  int                `json:"total"`
 	Limit  int                `json:"limit"`
 	Offset int                `json:"offset"`
+}
+
+type bangumiSubjectResponse struct {
+	Data   []bangumiSubject `json:"data"`
+	Total  int              `json:"total"`
+	Limit  int              `json:"limit"`
+	Offset int              `json:"offset"`
 }
 
 type bangumiCharacter struct {
@@ -29,4 +44,42 @@ type images struct {
 	Grid   string `json:"grid"`
 	Large  string `json:"large"`
 	Medium string `json:"medium"`
+}
+
+type bangumiSubject struct {
+	ID       int                `json:"id"`
+	Name     string             `json:"name"`
+	NameCN   string             `json:"name_cn"`
+	NameJP   string             `json:"name_jp"`
+	AirDate  string             `json:"air_date"`
+	Summary  string             `json:"summary"`
+	Images   images             `json:"images"`
+	Tags     []string           `json:"tags"`
+	MetaTags []any              `json:"meta_tags"`
+	NSFW     bool               `json:"nsfw"`
+	Infobox  []bangumiInfoboxKV `json:"infobox"`
+}
+
+type bangumiSubjectCharacterResponse struct {
+	Data   []bangumiSubjectCharacter `json:"data"`
+	Total  int                       `json:"total"`
+	Limit  int                       `json:"limit"`
+	Offset int                       `json:"offset"`
+}
+
+type bangumiSubjectCharacter struct {
+	ID       int      `json:"id"`
+	Name     string   `json:"name"`
+	NameCN   string   `json:"name_cn"`
+	NameJP   string   `json:"name_jp"`
+	Actor    string   `json:"actor"`
+	Relation string   `json:"relation"`
+	Role     string   `json:"role"`
+	Images   images   `json:"images"`
+	Tags     []string `json:"tags"`
+}
+
+type bangumiInfoboxKV struct {
+	Key   string `json:"key"`
+	Value any    `json:"value"`
 }
