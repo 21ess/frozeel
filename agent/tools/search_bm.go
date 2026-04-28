59e3bcd2daa776389f0150f86ac8d8b69b3b5c50
Lucid99 <yuhao21ess@outlook.com>
2026-04-28 20:33:51 +0800

    feat: add bangumi tools
    need fix
    
    Co-Authored-By: kjasn <iz.kjasn@icloud.com>

---
+++ agent/tools/search_bm.go
@@ -0,0 +1,36 @@
+// Package tools
+// Bangumi API 调用工具能力类
+package tools
+
+import (
+	"context"
+
+	"github.com/21ess/frozeel/provider/bangumi"
+	"github.com/cloudwego/eino/components/tool"
+	"github.com/cloudwego/eino/schema"
+)
+
+type BangumiSearchTool struct {
+	bmProvider *bangumi.BmProvider
+}
+
+func NewBangumiSearchTool(bmProvider *bangumi.BmProvider) *BangumiSearchTool {
+	return &BangumiSearchTool{
+		bmProvider: bmProvider,
+	}
+}
+
+func (b *BangumiSearchTool) InvokableRun(ctx context.Context, argumentsInJSON string, opts ...tool.Option) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BangumiSearchTool) Info(ctx context.Context) (*schema.ToolInfo, error) {
+	return &schema.ToolInfo{
+		Name: "search_bangumi_character",
+		Desc: "搜索 Bangumi acg 角色信息，包括角色名称、简介、性别、生日等",
+		ParamsOneOf: schema.NewParamsOneOfByParams(map[string]*schema.ParameterInfo{
+			// TODO
+		}),
+	}, nil
+}
